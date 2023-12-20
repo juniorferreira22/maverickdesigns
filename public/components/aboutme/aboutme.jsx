52fc412583bf08ed28386ecdeaffceada588385f
Junior Ferreira <jr.ferreira0018@gmail.com>
2023-12-20 14:37:20 -0300

    filling dummy texts

--- public/components/aboutme/aboutme.jsx
+++ public/components/aboutme/aboutme.jsx
@@ -10,11 +10,7 @@ export default function AboutMe() {
             <h1 className="aboutme__title" >SOBRE JUNIOR FERREIRA</h1>
             <div className="aboutme__firstsession">
                 <img className="aboutme__image" src="" alt="" />
-                <p>xxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx x
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
-                    xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxx x
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+                <p>Junior Ferreira, com 23 anos, é designer gráfico, web-designer, web-developer desde os 21 anos, e trabalha hoje exercendo a profissão de TI na secretaria da educação de onde mora e sendo freela de web-development onde mora.<br /> <br /> Junior estuda hoje um ensino superior em Engenharia de Software online. <br /> <br /> Seus maiores objetivos envolvem se aperfeiçoar nas áreas onde atua e trabalhar nos EUA como developer front-end. </p>
             </div>
             <h1 className="aboutme__secondtitle" >QUEM ESTÁ NO MEU TIME</h1>
 
@@ -24,15 +20,32 @@ export default function AboutMe() {
             </div>
             
             <h1 className="aboutme__secondtitle">O QUE EU POSSO FAZER POR VOCÊ</h1>
-            <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxx xxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxx xxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxxx 
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+                <ul>
+                    <li className="list-item">
+                        - Landing pages;
+                    </li>
+                    <br />
+                    <li className="list-item">
+                        - Figmas;
+                    </li>
+                    <br />
+                    <li className="list-item">
+                        - Thumbnails;
+                    </li>
+                    <br />
+                    <li className="list-item">
+                        - Capas para Facebook; 
+                    </li>
+                    <br />
+                    <li className="list-item">
+                        - Artes vetoriais; 
+                    </li>
+                    <br />
+                    <li className="list-item">
+                        - Design, Web-Design e Web-Development no geral.
+                    </li>
+                    
+                </ul>
             <h1 className="aboutme__secondtitle">COM QUE TECNOLOGIAS EU TRABALHO</h1>
             <Technologies/>
         </div>
